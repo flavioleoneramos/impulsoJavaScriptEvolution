3eef653ad338014426122ebdb5684fa402ea5536
flavioleoneramos <flavioleone8383@gmail.com>
2022-06-24 18:10:50 -0300

    flex

---
+++ flexBox/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+<body>
+  <section id="container">
+    <header id="cabecalho">
+      <section class="itensHeader wrap">
+        logo
+      </section>
+      <section class="itensHeader wrap">
+        menu
+      </section>
+      <section class="itensHeader wrap">
+        login/cadastro
+      </section>
+
+    </header>
+    <section id="corpo">
+    <section class="itensSection">
+        header corpo
+      </section>
+      <section class="itensSection">
+        corpo
+      </section>
+    </section>
+    <footer id="rodape">
+    <section class="itensFooter">
+        logo
+      </section>
+      <section class="itensFooter">
+        menu
+      </section>
+      <section class="itensFooter">
+        login/cadastro
+      </section>
+    </footer>
+  </section>
+</body>
+</html>
